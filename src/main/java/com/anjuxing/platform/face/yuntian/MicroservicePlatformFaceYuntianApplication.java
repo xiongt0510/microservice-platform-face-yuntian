6cb79a9d6ecf888cc4f379cfdc642adb479a4205
xiongt <changdexxy@163.com>
2018-09-06 12:02:10 +0800

    部署微服务

--- src/main/java/com/anjuxing/platform/face/yuntian/MicroservicePlatformFaceYuntianApplication.java
+++ src/main/java/com/anjuxing/platform/face/yuntian/MicroservicePlatformFaceYuntianApplication.java
@@ -2,6 +2,7 @@ package com.anjuxing.platform.face.yuntian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class MicroservicePlatformFaceYuntianApplication {
 
     public static void main(String [] args){
--- src/main/java/com/anjuxing/platform/face/yuntian/config/YunTianConfig.java
+++ src/main/java/com/anjuxing/platform/face/yuntian/config/YunTianConfig.java
@@ -2,6 +2,10 @@ package com.anjuxing.platform.face.yuntian.config;
 
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.cloud.netflix.feign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,9 +22,12 @@ public class YunTianConfig {
     @Primary
     @Configuration
     public static class EncoderSupportConfig {
+
+        @Autowired
+        private ObjectFactory<HttpMessageConverters> messageConverters;
         @Bean
         public Encoder feignFormEncoder() {
-            return new SpringFormEncoder();
+            return new SpringFormEncoder(new SpringEncoder(messageConverters));
         }
     }
 
